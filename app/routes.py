4e9abf259c4bf9f4e307dc29a546183b50c43efd
Matt Traudt <sirmatt@ksu.edu>
2020-04-14 19:30:23 -0400

    Beginner template stuff

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,22 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
-def index_():
+@app.route('/index')
+def index():
     user = {'username': 'MUTT'}
     i = len(app.config['SECRET_KEY'])
     return render_template('index.html', title='Home', user=user, i=i)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f'Login requested for {form.username.data}, '
+            f'remember_me {form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
