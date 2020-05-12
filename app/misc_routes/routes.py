9522ee81e284b2af6551125e40bcd127a8509239
Matt Traudt <sirmatt@ksu.edu>
2020-05-12 19:39:16 -0400

    Add beginning of FAQ

--- app/misc_routes/routes.py
+++ app/misc_routes/routes.py
@@ -5,3 +5,7 @@ from ..routes import my_render_template
 @bp.route('/')
 def index():
     return my_render_template('index.html', title='Home')
+
+@bp.route('/faq')
+def faq():
+    return my_render_template('faq.html', title='FAQ')
